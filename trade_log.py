a2fb064f2ab4ba3b4424717c7283569ca9bef809
VigneshSella <vsella2@illinois.edu>
2020-04-20 16:09:01 -0500

    updating trade_log

--- trade_log.py
+++ trade_log.py
@@ -3,13 +3,34 @@ Trade log class stores all trades on a CSV file and periodically updates them.
 It integrates with account class in order to update market value.
 """
 from account import Account
+import pandas as pd
 
 
 class TradeLog:
     def __init__(self, instance_name):
+        '''
+        Initializes the trade log for our trading sessions.
+
+        Parameters:
+            - instance_name: The name of our trading sessions
+
+        Class attributes:
+            - self.name:
+        '''
         self.name = instance_name
-        # initiate CSV file with name of instance
-        pass
+        self.df = pd.DataFrame(columns={
+            'ticker',
+            'time',
+            'date',
+            'action',
+            'price',
+            'num_shares',
+            'cost_basis'})
+        self.portfolio = pd.DataFrame(columns={
+            'ticker',
+            'num_shares',
+            'current_price'
+        })
 
     # Trading
     def trade(self, acnt, ticker, time, date, action, price, num_shares):
@@ -20,6 +41,16 @@ class TradeLog:
         if action == "sell":
             acnt.update(cost_basis, "sell")
             pass
-
         # store ticker, time, etc to CSV file
-        pass
+        self.df['ticker'] = ticker
+        self.df['time'] = time
+        self.df['date'] = date
+        self.df['action'] = action
+        self.df['price'] = price
+        self.df['num_shares'] = num_shares
+        self.df['cost_basis'] = cost_basis
+
+    # End of Run
+
+    def save_to_csv(self):
+        self.df.to_csv()
