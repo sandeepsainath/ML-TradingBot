41be37b04141ad767500e67cfae88a315683ec8f
VigneshSella <vsella2@illinois.edu>
2020-04-20 17:35:44 -0500

    added API key

--- run.py
+++ run.py
@@ -11,14 +11,15 @@ from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 
 API_URL
-API_KEY
+API_KEY = JLF3VYO6W156F6WT
 
 ts = TimeSeries(key=API_KEY, output_format='pandas')
 data_ts, meta_ts = ts.get_intraday(symbol='ticker', interval='1min', outputsize='full')
 
 ti = TechIndicators(key=API_KEY, output_format='pandas')
-period=60
-data_ti, meta_ti = ti.get_sma(symbol='ticker', interval='1min', time_period=period, series_type='close')
+period = 60
+data_ti, meta_ti = ti.get_sma(symbol='ticker', interval='1min',
+                              time_period=period, series_type='close')
 
 trade_log = TradeLog('trial1')
 account = Account(1000, trade_log)
