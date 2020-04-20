cd718ffd51c2ab8f7c47817005244a6dc83ae444
Sandeep Sainath <sandeepsainath@berkeley.edu>
2020-04-20 15:04:55 -0700

    updating run.py

--- run.py
+++ run.py
@@ -2,8 +2,12 @@
 The actual bot.
 """
 from account import Account
-from trade_log import TradeLog as tl
+from trade_log import TradeLog
+from security import Security
+
+import requests
+import pandas as pd
 
 
-trade_log = tl('trial1')
-acnt = Account(1000, trade_log)
+trade_log = TradeLog('trial1')
+account = Account(1000, trade_log)
