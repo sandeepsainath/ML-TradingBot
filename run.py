d594ee7e8e1ea9c1a5d3e71f52f863cccd3366a9
VigneshSella <vsella2@illinois.edu>
2020-04-20 17:55:23 -0500

    working data pulling
    Co-Authored-By: Sandeep Sainath <sandeepsainath@users.noreply.github.com>

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ API_KEY = "JLF3VYO6W156F6WT"
 
 # API stuff
 
-trade_log = TradeLog('trial1')
+'''trade_log = TradeLog('trial1')
 account = Account(1000, trade_log)
 
 ts = TimeSeries(key=API_KEY, output_format='pandas')
@@ -24,22 +24,22 @@ data_ts, meta_ts = ts.get_intraday(symbol='ticker', interval='1min', outputsize=
 ti = TechIndicators(key=API_KEY, output_format='pandas')
 period = 60
 data_ti, meta_ti = ti.get_sma(symbol='ticker', interval='1min',
-                              time_period=period, series_type='close')
+                              time_period=period, series_type='close')'''
 
 
 ##############
 
 API_URL = "https://www.alphavantage.co/query"
-symbol = 'SMBL'
+symbol = 'MSFT'
 
 data = {"function": "TIME_SERIES_DAILY",
         "symbol": symbol,
         "outputsize": "full",
         "datatype": "json",
-        "apikey": "your_api_key"}
+        "apikey": API_KEY}
 
 response = requests.get(API_URL, data)
-response_json = response.json()  # maybe redundant
+response_json = response.json()
 
 data = pd.DataFrame.from_dict(
     response_json['Time Series (Daily)'], orient='index').sort_index(axis=1)
@@ -49,5 +49,5 @@ data = data.rename(columns={'1. open': 'Open',
                             '4. close': 'Close',
                             '5. adjusted close': 'AdjClose',
                             '6. volume': 'Volume'})
-data = data[['Open', 'High', 'Low', 'Close', 'AdjClose', 'Volume']]
-data.tail()  # check OK or not
+
+print(data)
