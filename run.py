e22f41b7807e9355730c0085ee58726c9177f55c
VigneshSella <vsella2@illinois.edu>
2020-04-20 13:07:00 -0500

    adding dependency tracking

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 """
 The actual bot.
 """
-import account
-import trade_log as tl
+from account import Account
+from trade_log import Trade_Log as tl
+import requests
 
 trade_log = tl('trial1')
-acnt = account(1000, trade_log)
+acnt = Account(1000, trade_log)
+
+response = requests.get('https://httpbin.org/ip')
+
+print('Your IP is {0}'.format(response.json()['origin']))
