0710301bd1485edd96a0ec390c6eaeaea7c1449b
VigneshSella <vsella2@illinois.edu>
2020-04-20 17:10:34 -0500

    test

--- testing.py
+++ testing.py
@@ -4,3 +4,6 @@ from trade_log import TradeLog as tl
 
 trade_log = tl('test1')
 acnt = Account(1000, trade_log)
+
+trade_log.trade(acnt, 'AAPL', 1200, '04/20/2000', 'buy', 100, 10)
+print(trade_log.df)
