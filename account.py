b396647894e2cc469a6fa4870e2607908a8ef9b7
VigneshSella <vsella2@illinois.edu>
2020-04-20 00:43:38 -0500

    created account class

---
+++ account.py
@@ -0,0 +1,35 @@
+class Account:
+    def __init__(self, cash, trade_log):
+        self.cash = cash
+        self.market_val = 0
+        self.trade_log = trade_log
+
+    # Getters
+    def available_cash(self):
+        return self.cash
+
+    def get_market_value(self):
+        return self.market_val
+
+    # Accessors
+    def set_market_value(self):
+        # trade_log would be an object, need to define it previously and access
+        # here
+        # market_val = shares owned*current price
+        pass
+
+    def update(self, amount, action):
+        if action == "buy":
+            self.cash -= amount
+            self.set_market_value()
+        if action == "sell":
+            self.cash += amount
+            self.set_market_value()
+
+    # Status
+    def solvent(self):
+        if self.market_val + self.cash > 0:
+            return True
+        else:
+            raise RuntimeError('Account is no longer solvent')
+            return False
