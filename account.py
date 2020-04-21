7f9a9ae70ead8d8b690905fbb98d9f05dcc96ef2
VigneshSella <vsella2@illinois.edu>
2020-04-20 22:25:30 -0500

    finished 'set_market_value' function, and added one error handling case

--- account.py
+++ account.py
@@ -6,10 +6,10 @@ The class integrates with the trade log class in order to check market value.
 
 
 class Account:
-    def __init__(self, cash, trade_log):
+    def __init__(self, cash):
         self.cash = cash
         self.market_val = 0
-        self.trade_log = trade_log
+        # self.trade_log = trade_log
 
     # Getters
     def available_cash(self):
@@ -19,14 +19,16 @@ class Account:
         return self.market_val
 
     # Accessors
-    def set_market_value(self):
-        # trade_log would be an object, need to define it previously and access
-        # here
-        # market_val = shares owned*current price
+    def set_market_value(self, market_value):
+        # trade_log function 'portfolio_to_value' needs to be used to get
+        # market value
+        self.market_val = market_value
         pass
 
     def update(self, amount, action):
         if action == "buy":
+            if self.cash - amount < 0:
+                raise Exception("Account's cash balance cannot go negative")
             self.cash -= amount
             self.set_market_value()
         if action == "sell":
